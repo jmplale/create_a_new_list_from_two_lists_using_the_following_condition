cd4deeaef3baf18762199e315afcd1575a4908e5
johnmichaelpplale <johnmichaelpplale@gmail.com>
2024-01-22 15:57:41 +0800

    for loop for bracket_two

--- exercise 10.py
+++ exercise 10.py
@@ -3,7 +3,12 @@ def combine(bracket_one, bracket_two):
     # make an variable with empty bracket
     new_bracket = []
 
-    # for loop for num_one
+    # for loop for bracket_one
     for number in bracket_one:
         if number % 2 != 0:
+            new_bracket.append(number)
+        
+    # for loop for bracket_two
+    for number in bracket_two:
+        if number % 2 == 0:
             new_bracket.append(number)
